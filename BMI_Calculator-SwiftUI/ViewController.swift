073ea167a80071d70dd35d97c2e70d6e8a22c35a
Sweta <vanjarasweta@gmail.com>
2026-05-04 12:23:54 +0530

    Initial Commit

---
+++ BMI_Calculator-SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_Calculator-SwiftUI
+//
+//  Created by Shweta Aadatiya on 04/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
